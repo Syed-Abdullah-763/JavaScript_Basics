b8d45fd0bf195aede3bf103c39dc02e964d38b28
Syed Abdullah <syed.abdullah7638@gmail.com>
2024-12-19 00:35:36 +0500

    Operators & Condition

---
+++ app.js
@@ -0,0 +1,42 @@
+
+// *********Operators**********
+
+
+// var a = "10";
+// var b = "20";
+// console.log(a + b);
+// console.log(a - b);
+// console.log("80" - 20 + 10);
+// console.log("80" + 20 + 10);
+// console.log(80 + 20 + "10");
+// console.log(80 + 20 - "10");
+
+
+
+
+// var a = 10;
+
+// a += 1;
+// a++
+// ++a
+// console.log(a++, a)
+// console.log(++a, a)
+// console.log("a=>", a)
+
+// var myName = "Syed";
+// myName += " " + "Abdullah";
+// console.log(myName);
+
+// var a = 8;
+// a++ + a++ + ++a - --a - ++a + a++ + --a
+// console.log(a++ + a++ + ++a - --a - ++a + a++ + --a)
+
+
+// ******Condition********
+var age = 19;
+
+if (age == 18) {
+    console.log("Allow")
+} else {
+    console.log("Don't Allow");
+}
