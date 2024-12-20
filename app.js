175d3d8b45694e573839ea32da757c387d466eda
Syed Abdullah <syed.abdullah7638@gmail.com>
2024-12-21 00:29:52 +0500

    condition Chaining

--- app.js
+++ app.js
@@ -33,10 +33,116 @@
 
 
 // ******Condition********
-var age = 19;
+// var age = 19;
 
-if (age == 18) {
-    console.log("Allow")
+// if (age == 18) {
+//     console.log("Allow")
+// } else {
+//     console.log("Don't Allow");
+// }
+
+
+// var age = "18";
+
+// if (age == 18) {
+//     console.log("You can use thus web.")
+// } else {
+//     console.log("You can't use thus web.")
+// }
+
+// if (age === 18) {
+//     console.log("You can use thus web.")
+// } else {
+//     console.log("You can't use thus web.")
+// }
+
+
+
+// var num1 = "100";
+
+// if (typeof(num1) == "number") {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+// num1 = 100;
+// if (typeof(num1) == "number") {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+
+// var age = prompt("Enter Your Age");
+
+// if (age == 18) {
+//     console.log(true);
+    
+// } else {
+//     console.log(false);
+// }
+
+
+// var age = +prompt("Enter Your Age");
+
+// if (age === 18) {
+//     console.log(true);
+    
+// } else {
+//     console.log(false);
+// }
+
+
+// 18 , 20 , 25 , 30 => allow
+// var age = 20;
+// if (age == 18) {
+//   console.log("allow");
+// } else if (age == 20) {
+//   console.log("allow");
+// } else if (age == 25) {
+//   console.log("allow");
+// } else {
+//   console.log("not allow");
+// }
+
+// if(age == 20){
+//   console.log("allow")
+// }else{
+//   console.log("not allow")
+// }
+
+// var gender = "male";
+// if (gender == "male") {
+//   console.log("larka hai");
+// } else {
+//   console.log("pata nahi");
+// }
+
+
+// var age = prompt("enter your age");
+// if (age >=  18) {
+//   console.log("ALLOW");
+// } else {
+//   console.log("not allow");
+// }
+
+
+
+// ----calculator-----
+
+var num1 = +prompt("Enyter First Number", "Type Here");
+var opt = prompt("Enyter Function", "+ , - , * , /");
+var num2 = +prompt("Enyter Second Number", "Type Here");
+
+if (opt == "+") {
+   console.log(num1 + num2);
+} else if (opt == "-") {
+    console.log(num1 - num2);
+} else if (opt == "*") {
+    console.log(num1 * num2);
+} else if (opt == "/") {
+    console.log(num1 / num2);
 } else {
-    console.log("Don't Allow");
+    console.log("SYNTAX ERROR");
 }
